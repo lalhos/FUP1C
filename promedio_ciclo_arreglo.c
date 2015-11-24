b5897c055cab676dea74ade79c1d10cf2e1b2a05
lalhos <lalhos22@gmail.com>
2015-11-23 20:29:47 -0600

    trabajos de programacion

---
+++ promedio_ciclo_arreglo.c
@@ -0,0 +1,60 @@
+#include<stdio.h>
+
+int suma;
+int numero;
+int i;
+int arreglo[10];
+float prome;
+
+
+int main()
+{
+	
+	system("color 0e");
+	arreglo[i];
+	
+    arreglo[0];
+	arreglo[1];
+	arreglo[2];
+	arreglo[3];
+	arreglo[4];
+	arreglo[5];
+	arreglo[6];
+	arreglo[7];
+	arreglo[8];
+	arreglo[9];
+	
+
+	printf("programa que nos de el promedio de 10 calificaciones y la moda :\n");
+	printf("\n");
+	printf("digita las 10 calificaciones:\n",numero+1);
+	printf("\n");
+	printf("ahora mostraremos el numero que mayor se repite:\n");
+	
+	
+	suma = 0;
+
+	for(i = 1; i <= 10; i++)
+	{
+			arreglo[i];
+	
+	}
+	for(i = 1; i <= 10; i++)
+	{
+		scanf("%d",&numero);
+		suma= suma + numero;
+	    arreglo[i];
+	}
+	if()
+	
+	prome =(float) suma /10;
+    printf("el promedio final es: >>>>>>>%f\n",prome);
+    printf("\n");
+    printf("ERES UN CHAMPIONS :)\n");
+
+
+    system("pause");
+	
+}
+
+
